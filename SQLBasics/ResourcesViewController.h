7a9c80c93d47f29675e2e25f9be6a2bb075bff37
anilmahadev <anil_mahadev@icloud.com>
2014-01-30 01:22:42 +0530

    completed till chapters, next need to complete resources section and e-mail.

---
+++ SQLBasics/ResourcesViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResourcesViewController.h
+//  SQLBasics
+//
+//  Created by Anil Mahadev on 29/01/14.
+//  Copyright (c) 2014 Anil Mahadev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResourcesViewController : UIViewController
+
+@end
---
+++ SQLBasics/ResourcesViewController.m
@@ -0,0 +1,38 @@
+//
+//  ResourcesViewController.m
+//  SQLBasics
+//
+//  Created by Anil Mahadev on 29/01/14.
+//  Copyright (c) 2014 Anil Mahadev. All rights reserved.
+//
+
+#import "ResourcesViewController.h"
+
+@interface ResourcesViewController ()
+
+@end
+
+@implementation ResourcesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
